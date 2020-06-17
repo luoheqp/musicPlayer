8c0bcd027b96561bd06633952a7ed36479b3b896
leoljustsave <leoljustsave@gmail.com>
2020-06-17 08:35:29 +0800

    save

--- src/pages/Index/components/MusicProgress/index.jsx
+++ src/pages/Index/components/MusicProgress/index.jsx
@@ -1,29 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useSelector } from "react-redux";
-import { MusicProgressContent } from "./style";
+import { MusicProgressContent, MovePoint } from "./style";
 
 const MusicProgress = (props) => {
   const duration = useSelector(({ player }) => player.duration);
   const currentTime = useSelector(({ player }) => player.currentTime);
 
   const [progress, setProgress] = useState(0);
-  const [isMouseDown, setIsMouseDown] = useState(false);
-  const [mouseDownPos, setMouseDownPos] = useState(0);
+  const [startPosX, setStartPosX] = useState(0);
+  const [isDraging, setIsDraging] = useState(false);
 
-  const handleMouseDownInProgress = ({ clientX }) => {
-    setMouseDownPos(clientX);
-    setIsMouseDown(true);
+  const progressBar = useRef();
+
+  // 计算进度 同步修改进度条
+  const handleChangeAudioProgress = ({ pageX }) => {
+    const { offsetLeft, clientWidth } = progressBar.current;
+    let offsetX = pageX - startPosX - offsetLeft;
+    let progress = offsetX / clientWidth;
+    progress = progress > 1 ? 1 : progress < 0 ? 0 : progress;
+    setProgress(progress * 100);
   };
 
-  const handleMouseMoveInProgress = ({ clientX }) => {
-    if (!isMouseDown) {
-      return;
-    }
-    console.log(clientX - mouseDownPos);
-  };
+  // 鼠标按下事件
+  const handleMouseDownInProgress = ({ pageX }) => {
+    const { offsetLeft, clientWidth } = progressBar.current;
+    let startPosX = pageX - offsetLeft;
+    setProgress(startPosX / clientWidth);
+    setIsDraging(true);
+    setStartPosX(startPosX);
 
-  const handleMouseUpInProgress = (e) => {
-    setIsMouseDown(false);
+    document.addEventListener("mousemove", handleChangeAudioProgress);
+    document.addEventListener("mouseup", () => {
+      // 重置状态并清除事件
+      setIsDraging(false);
+      document.removeEventListener("mousemove", handleChangeAudioProgress);
+    });
   };
 
   useEffect(() => {
@@ -33,14 +44,16 @@ const MusicProgress = (props) => {
   }, [currentTime, duration]);
 
   return (
-    <MusicProgressContent
-      progress={progress}
-      onMouseDown={handleMouseDownInProgress}
-      onMouseMove={handleMouseMoveInProgress}
-      onMouseUp={handleMouseUpInProgress}
-    >
-      <div className="move-point"></div>
-    </MusicProgressContent>
+    <>
+      <MusicProgressContent
+        progress={progress}
+        onMouseDown={handleMouseDownInProgress}
+        isDraging={isDraging}
+        ref={progressBar}
+      >
+        <MovePoint progress={progress}></MovePoint>
+      </MusicProgressContent>
+    </>
   );
 };
 
--- src/pages/Index/components/MusicProgress/style.jsx
+++ src/pages/Index/components/MusicProgress/style.jsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const MusicProgressContent = styled.div`
   width: 100%;
-  height: 3px;
+  height: 4px;
+  border-radius: 2px;
   background-image: linear-gradient(
     to right,
     rgba(51, 51, 51, 1) ${(props) => `${props.progress}%`},
@@ -14,20 +15,21 @@ export const MusicProgressContent = styled.div`
 
   &:hover {
     .move-point {
-      width: 5px;
-      height: 5px;
+      width: 10px;
+      height: 10px;
     }
   }
+`;
 
-  .move-point {
-    width: 0px;
-    height: 0px;
-    background-color: #333;
-    border-radius: 50%;
-    transform: translate(-50%, -50%);
-    transition: all 0.1s linear;
-    position: absolute;
-    top: 50%;
-    left: ${(props) => `${props.progress}%`};
-  }
+export const MovePoint = styled.div`
+  width: ${({ isDraging }) => `${isDraging ? "10px" : "0px"}`};
+  height: ${({ isDraging }) => `${isDraging ? "10px" : "0px"}`};
+  background-color: #333;
+  border-radius: 50%;
+  transform: translate(-50%, -50%);
+  transition: width 0.1s cubic-bezier(1, 0, 0, 2),
+    height 0.1s cubic-bezier(1, 0, 0, 2);
+  position: absolute;
+  top: 50%;
+  left: ${(props) => `${props.progress}%`};
 `;
