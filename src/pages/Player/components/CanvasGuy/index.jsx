2c96d29624b127bcf903eddbf115daa88cc35b39
chenyipeng <chenyipeng@lizhi.fm>
2020-06-22 17:24:53 +0800

    update 添加音频可视化

--- src/pages/Player/components/CanvasGuy/index.jsx
+++ src/pages/Player/components/CanvasGuy/index.jsx
@@ -3,82 +3,78 @@ import { CanvasGuyContent } from "./style";
 import { useSelector } from "react-redux";
 
 const CanvasGuy = (props) => {
+  const LINE_CYCLE_COUNT = 200;
+
   const source = useSelector(({ player }) => player.source);
   const mediaPlayNow = useSelector(({ player }) => player.mediaPlayNow);
 
-  const [audioCtx, setAudioCtx] = useState(undefined);
-  const [analyser, setAnalyser] = useState(undefined);
-  const [grd, setGrad] = useState(undefined);
-  const [grd2, setGrad2] = useState(undefined);
+  const [audioContext, setAudioContext] = useState();
 
+  const outerBoxRef = useRef();
   const canvasRef = useRef();
 
   const handleAudioAnimation = (ctx, analyser) => {
-    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    console.log("object");
+    const { current } = canvasRef;
+
+    ctx.clearRect(0, 0, current.width, current.height);
 
     let dataArray = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(dataArray);
 
+    ctx.save();
     ctx.beginPath();
-    for (let i = 0; i < 200; i++) {
+    ctx.translate(current.offsetWidth / 2, current.offsetHeight / 2);
+    for (let i = 0; i < LINE_CYCLE_COUNT; i++) {
+      ctx.rotate((i * Math.PI) / LINE_CYCLE_COUNT);
       let value = dataArray[6 * i];
-      ctx.fillStyle = grd;
-      ctx.fillRect(i * 5, 300, 2, -value + 1);
-      ctx.fillRect(i * 5, 280 - value, 2, 0);
-      ctx.fillStyle = grd2;
-      ctx.fillRect(i * 5, 300, 2, value + 1);
-      ctx.fillRect(i * 5, 320 + value, 2, 0);
+      ctx.fillStyle = "#333";
+      ctx.fillRect(0, 100, 2, value / 10);
     }
 
-    requestAnimationFrame(handleAudioAnimation(ctx, analyser));
+    ctx.restore();
+
+    requestAnimationFrame(() => handleAudioAnimation(ctx, analyser));
   };
 
-  const handleInitAudioContent = useCallback(() => {
+  const handleInitAudioContext = (audioCtx) => {
     // 创建 AnalyserNode 节点
     let analyser = audioCtx.createAnalyser();
-    analyser.fftSize = 256;
-
-    setAnalyser(analyser);
 
     // 链接节点
     let audioSource = audioCtx.createMediaElementSource(source);
     audioSource.connect(analyser);
     audioSource.connect(audioCtx.destination);
 
-    let ctx = canvasRef.current.getContext("2d");
+    const { current } = canvasRef;
+    let ctx = current.getContext("2d");
     ctx.lineWidth = 2;
-    let grd = ctx.createLinearGradient(0, 0, 600, 0);
-    grd.addColorStop(0, "#00d0ff");
-    grd.addColorStop(1, "#eee");
-
-    setGrad(grd);
-
-    let grd2 = ctx.createLinearGradient(0, 0, 600, 0);
-    grd2.addColorStop(0, "#fff");
-    grd2.addColorStop(1, "#e720ee");
-
-    setGrad2(grd2);
 
     handleAudioAnimation(ctx, analyser);
-  }, [audioCtx, source]);
+  };
+
+  const handleWindowResize = () => {
+    const { current } = outerBoxRef;
+    canvasRef.current.width = current.offsetWidth;
+    canvasRef.current.height = current.offsetHeight;
+  };
 
   useEffect(() => {
-    if (audioCtx && source) {
-      handleInitAudioContent();
-    }
-  }, [audioCtx, source, handleInitAudioContent]);
-
-  useEffect(() => {
-    if (mediaPlayNow) {
-      console.log("object");
+    if (mediaPlayNow.name && source && !audioContext) {
       let audioContext = new (window.AudioContext ||
         window.webkitAudioContext)();
-      setAudioCtx(audioContext);
+      setAudioContext(audioContext);
+      handleInitAudioContext(audioContext);
     }
-  }, [mediaPlayNow]);
+  }, [mediaPlayNow, source, audioContext]);
+
+  useEffect(() => {
+    handleWindowResize();
+    window.onresize = handleWindowResize();
+  }, []);
 
   return (
-    <CanvasGuyContent>
+    <CanvasGuyContent ref={outerBoxRef}>
       <canvas ref={canvasRef}></canvas>
     </CanvasGuyContent>
   );
--- src/pages/Player/components/CanvasGuy/style.jsx
+++ src/pages/Player/components/CanvasGuy/style.jsx
@@ -1,3 +1,6 @@
 import styled from "styled-components";
 
-export const CanvasGuyContent = styled.div``;
+export const CanvasGuyContent = styled.div`
+  width: 100%;
+  height: 100%;
+`;
