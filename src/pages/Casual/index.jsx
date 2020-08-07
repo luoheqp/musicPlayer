f4eb3ae23593c2affa14c80be5a6f0e42703d76e
chenyipeng <chenyipeng@lizhi.fm>
2020-08-07 16:38:01 +0800

    refactor : 抽离 Casual/TheBtn 组件

--- src/pages/Casual/index.jsx
+++ src/pages/Casual/index.jsx
@@ -1,61 +1,23 @@
-import React, { useEffect, useRef, useState, useCallback } from "react";
+import React from "react";
 import useLoading from "@/hooks/useLoading";
-import useInterval from "@/hooks/useInterval";
-import { sleep } from "@/utils";
-import anime from "animejs";
 import { useSelector, useDispatch } from "react-redux";
 import { handleGetPersonalFmList } from "@r/casual/action.js";
 
-import { CasualContent, TheBtn } from "./style.jsx";
-
-let VOICE_FREQUENCY = [196, 220, 246, 261, 293, 329, 349, 392];
+// components
+import { CasualContent } from "./style.jsx";
+import TheBtn from "./components/TheBtn";
 
 const Casual = () => {
   const dispatch = useDispatch();
+
+  const casualList = useSelector(({ casual }) => casual.casualList);
+
   const { LoadingDom, toggleLoading } = useLoading({
     init: false,
     position: "top",
   });
 
-  const audioCtx = useRef();
-  const theBtnRef = useRef();
-
-  const casualList = useSelector(({ casual }) => casual.casualList);
-
-  const [isTouching, setIsTouching] = useState(false);
-
-  const start = useRef(0);
-  const direction = useRef(1);
-
-  // 状态重置
-  const handleResetConfig = () => {
-    setIsTouching(false);
-    start.current = 0;
-    direction.current = 1;
-  };
-
-  // 触发声音
-  const handleMakeVoice = useCallback((frequency) => {
-    let oscillator = audioCtx.current.createOscillator();
-    let gainNode = audioCtx.current.createGain();
-
-    oscillator.connect(gainNode);
-    gainNode.connect(audioCtx.current.destination);
-
-    oscillator.type = "sine";
-    oscillator.frequency.value = frequency;
-    oscillator.start(audioCtx.current.currentTime);
-    // 1秒内声音慢慢降低，是个不错的停止声音的方法
-    gainNode.gain.exponentialRampToValueAtTime(
-      0.001,
-      audioCtx.current.currentTime + 1
-    );
-    // 1秒后完全停止声音
-    oscillator.stop(audioCtx.current.currentTime + 1);
-  }, []);
-
-  // 获取随机的歌曲
-  const handleGetRandomSong = useCallback(async () => {
+  const handleGetPersonalFm = async () => {
     toggleLoading(true);
 
     try {
@@ -63,63 +25,11 @@ const Casual = () => {
     } catch (e) {}
 
     toggleLoading(false);
-
-    anime({
-      targets: theBtnRef.current,
-      scale: 10,
-      opacity: 0,
-      easing: "easeInOutQuad",
-      duration: 500,
-    });
-  }, []);
-
-  // 音频播放 interval
-  useInterval(
-    () => {
-      var frequency = VOICE_FREQUENCY[start.current];
-      if (!frequency) {
-        handleResetConfig();
-        handleGetRandomSong();
-        return;
-      }
-      start.current = start.current + direction.current;
-
-      anime({
-        targets: theBtnRef.current,
-        scale: 1.2,
-        direction: "alternate",
-        easing: "easeInOutQuad",
-        duration: 100,
-      });
-
-      handleMakeVoice(frequency);
-    },
-    isTouching ? 300 : null
-  );
-
-  // touch start
-  const handleTouchStart = () => {
-    setIsTouching(true);
   };
-
-  // touch end
-  const handleTouchEnd = () => {
-    handleResetConfig();
-  };
-
-  useEffect(() => {
-    audioCtx.current = new AudioContext();
-  }, []);
 
   return (
     <CasualContent>
-      <TheBtn
-        onTouchStart={handleTouchStart}
-        onTouchEnd={handleTouchEnd}
-        ref={theBtnRef}
-      >
-        <i className="iconfont icon-music-line"></i>
-      </TheBtn>
+      <TheBtn musicPowerFull={handleGetPersonalFm} />
       {LoadingDom}
     </CasualContent>
   );
--- src/pages/Casual/style.jsx
+++ src/pages/Casual/style.jsx
@@ -7,18 +7,3 @@ export const CasualContent = styled.div`
   align-items: center;
   justify-content: center;
 `;
-
-export const TheBtn = styled.div`
-  width: 80px;
-  height: 80px;
-  border-radius: 50%;
-  background-color: #333;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  .iconfont {
-    color: #fff;
-    font-size: 36px;
-  }
-`;
