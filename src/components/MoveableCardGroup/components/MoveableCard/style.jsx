b1d69b1e732d9e0cf7b68c855481f500a29f730b
leoljustsave <leoljustsave@gmail.com>
2020-08-09 17:21:47 +0800

    feat : 完善 moveableCardGroup

--- src/components/MoveableCardGroup/components/MoveableCard/style.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const MoveableCardContent = styled.div`
-  width: 300px;
-  height: 400px;
-  border: 1px solid #333;
-  border-radius: 10px;
-`;
--- src/components/MoveableCardGroup/style.jsx
+++ src/components/MoveableCardGroup/style.jsx
@@ -9,6 +9,8 @@ export const MoveableCardGroupContent = styled.div`
   justify-content: center;
 
   .card-contant {
+    width: 300px;
+    height: 400px;
     position: absolute;
 
     &:nth-child(1) {
@@ -17,7 +19,95 @@ export const MoveableCardGroupContent = styled.div`
 
     &:nth-child(2) {
       z-index: 0;
-      
+      transform: scale(0.9) translateY(-20px);
+      transform-origin: top;
+      opacity: 0.4;
+    }
+  }
+`;
+
+export const Card = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  position: absolute;
+  overflow: hidden;
+  background-color: #fff;
+  box-shadow: 0px 0px 10px 0px #ccc;
+
+  .cover {
+    height: 200px;
+    background-image: url(${({ src }) => src});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+
+  > .name {
+    font-size: 24px;
+    line-height: 24px;
+    padding: 10px;
+    text-align: center;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
+
+export const AuthorContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 10px;
+
+  .avatar {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    background-image: url(${({ src }) => src});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    border: 5px solid #fff;
+    margin-top: -60px;
+    margin-bottom: 0px;
+  }
+
+  > .name {
+    font-size: 12px;
+  }
+`;
+
+export const Player = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .controller {
+    .player {
+      font-size: 20px;
+      padding: 10px;
+    }
+  }
+
+  .progress {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .time {
+      font-size: 12px;
+      margin-bottom: 10px;
+    }
+
+    .line {
+      width: 80%;
+      height: 2px;
+      background-color: #ccc;
+      border-radius: 1px;
     }
   }
 `;
