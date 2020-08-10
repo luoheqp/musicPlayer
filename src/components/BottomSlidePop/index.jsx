65ced917b8060488cc462226d6395813b6ba4a4c
chenyipeng <chenyipeng@lizhi.fm>
2020-08-10 20:27:19 +0800

    feat : init 底部上滑弹窗

---
+++ src/components/BottomSlidePop/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { SlidePopContent, SongCollectList } from "./style";
+import { useSelector } from "react-redux";
+
+// components
+import { CSSTransition } from "react-transition-group";
+
+const SlidePop = ({ title, state, close, clickTrigger }) => {
+  const songList = useSelector(({ common }) => common.songList);
+
+  const handleOnClick = (item) => {
+    clickTrigger(item.id);
+    close();
+  };
+
+  return (
+    <CSSTransition in={state} timeout={300} unmountOnExit={true}>
+      {(value) => (
+        <SlidePopContent onClick={close} state={value}>
+          <div className={`main ${value}`}>
+            <h3 className="title">{title}</h3>
+            <SongCollectList>
+              {songList?.map((item, index) => (
+                <div
+                  className="item"
+                  key={index}
+                  onClick={() => handleOnClick(item)}
+                >
+                  <img src={`${item.coverImgUrl}?param=200y200`} alt="" />
+                  <p className="name">{item.name}</p>
+                </div>
+              ))}
+            </SongCollectList>
+          </div>
+        </SlidePopContent>
+      )}
+    </CSSTransition>
+  );
+};
+
+export default SlidePop;
